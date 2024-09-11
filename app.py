7b6428af55f8ee99ed2d048313a82c0843789bd3
Richard Leyshon <leyshonrr@hotmail.co.uk>
2024-09-11 21:54:00 +0100

    refactor: Move stream initial messages to constants.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""Main app file for the Jungle Quest game."""
+
 from pathlib import Path
 
 from faicons import icon_svg
@@ -5,48 +7,7 @@ import openai
 from shiny import App, ui, reactive
 from shinyswatch import theme
 
-
-
-_SYSTEM_MSG = """
-You are the guide of a 'choose your own adventure'- style game: a mystical
-journey through the Amazon Rainforest. Your job is to create compelling
-outcomes that correspond with the player's choices. You must navigate the
-player through challenges, providing choices, and consequences, dynamically
-adapting the tale based on the player's inputs. Your goal is to create a
-branching narrative experience where each of the player's choices leads to
-a new path, ultimately determining their fate. The player's goal is to find
-the lost crown of Quetzalcoatl.
-
-Here are some rules to follow:
-1. Always wait for the player to respond with their input before providing
-any choices. Never provide the player's input yourself. This is most
-important.
-2. Ask the player to provide a name, gender and race.
-3. Ask the player to choose from a selection of weapons that will be used
-later in the game.
-4. Have a few paths that lead to success. 
-5. Have some paths that lead to death.
-6. Whether or not the game results in success or death, the response must
-include the text "The End...", I will search for this text to end the game.
-"""
-
-WELCOME_MSG = """
-Welcome to the Amazon Rainforest, adventurer! Your mission is to find the
-lost Crown of Quetzalcoatl:\n
-<div style="display: grid; place-items: center;"><img src="https://i.imgur.com/Fxa7p1D.jpeg" width=60%/></div>\n
-However, many challenges stand in your way. Are you brave enough, strong
-enough and clever enough to overcome the perils of the jungle and secure
-the crown?
-
-Before we begin our journey, choose your name, gender and race. Choose a
-weapon to bring with you. Choose wisely, as the way ahead is filled with
-many dangers.
-
-"""
-# compose the messages log
-_SYS = {"role": "system", "content": _SYSTEM_MSG}
-_WELCOME = {"role": "assistant", "content": WELCOME_MSG}
-stream = [_SYS, _WELCOME]
+from constants import WELCOME_MSG, stream
 
 # Create a welcome message
 welcome = ui.markdown(
